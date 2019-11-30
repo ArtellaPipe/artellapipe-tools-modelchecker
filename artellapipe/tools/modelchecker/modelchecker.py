620f1da0db2b2710a3c3bca629b739a44ccbaa87
Tomi Poveda <tpovedatd@gmail.com>
2019-11-30 22:06:37 +0100

    Removed unnecessary code

--- artellapipe/tools/modelchecker/modelchecker.py
+++ artellapipe/tools/modelchecker/modelchecker.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Module that contains publisher implementation for Artella
+Module that contains model checker implementation for Artella
 """
 
 from __future__ import print_function, division, absolute_import
@@ -20,20 +20,3 @@ class ArtellaModelChecker(artellapipe.PyblishTool, object):
     def __init__(self, project, config):
 
         super(ArtellaModelChecker, self).__init__(project=project, config=config)
-
-
-# def run(project):
-#
-#     if tp.is_maya():
-#         pyblish.api.register_host('maya')
-#     elif tp.is_houdini():
-#         pyblish.api.register_host('houdini')
-#
-#     win = ArtellaPublisher(project=project)
-#     win.show()
-
-#
-#     pyblish_win = pyblish_lite.show()
-#     win.set_pyblish_window(pyblish_win)
-#
-#     return win
